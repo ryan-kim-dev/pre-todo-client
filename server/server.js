ad41b07ef6fc2ecb3a052a14cfb9e88618f5bf15
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-08-22 10:50:52 +0900

    feat: Implement client & server

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+app.post('/upload/post', async (req, res) => {
+  res.send('success');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
